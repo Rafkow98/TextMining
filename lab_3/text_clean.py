ee8f70fe3422c40e874ab9d1f5590594a11536b4
Rafkow98 <rafal.kowalski3@edu.uekat.pl>
2022-03-28 19:05:34 +0200

    On branch lab_3  Changes to be committed: 	new file:   data/Fake.csv 	new file:   data/True.csv 	new file:   file_read.py 	new file:   main.py 	new file:   text_clean.py 	new file:   text_stem.py 	new file:   wordcloud.png

---
+++ lab_3/text_clean.py
@@ -0,0 +1,14 @@
+import re
+
+
+def cleanText(text):
+    emot = ' '.join([str(i) for i in
+                          re.findall(r'(?::\)|;\)|;\(|:>|:<|;<|:-\)|;-\))', text)])
+    t1 = re.sub(r'(?::\)|;\)|;\(|:>|:<|;<|:-\)|;-\))', '', text)
+    t2 = t1.lower()
+    t3 = re.sub(r'\d', '', t2)
+    t4 = re.sub(r'<.*?>', '', t3)
+    t5 = re.sub(' +', ' ', t4)
+    t6 = re.sub(r'[,;.]', '', t5)
+    res = t6 + emot
+    return res
