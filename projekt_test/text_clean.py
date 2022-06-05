13fd9f052485cc8444a1fbbc4565e1eb8f1d6af3
Rafkow98 <rafal.kowalski3@edu.uekat.pl>
2022-06-05 21:27:07 +0200

    On branch project  Changes to be committed: 	new file:   data/SMS_test.csv new file:   data/SMS_train.csv 	new file:   main.py 	new file:   stopwords.py 	new file:   text_clean.py 	new file:   text_stem.py 	new file:   tokenizer.py 	new file:   visualisation.py
     Changes not staged for commit:
    	modified:   ../lab_4/tokenizer.py
    
     Untracked files:
    	../elllllllllllllo/
    	../eloapp/
    	../hindusso/
    	../lab_1/
    	../lab_12/
    	../lab_1_/.idea/vcs.xml
    	../lab_3/.idea/
    	../lab_3/__pycache__/
    	../lab_4/.idea/
    	../lab_4/__pycache__/
    	../person_detection/
    	.idea/
    	__pycache__/
    	../pythonProject/
    ../pythonProject1.zip
    	../pythonProject1/
    	../pythonProject2/
    	../zad_1/

---
+++ projekt_test/text_clean.py
@@ -0,0 +1,14 @@
+import re
+
+
+def cleanText(text: str):
+    emot = ' '.join([str(i) for i in
+                    re.findall(r'(?::\)|;\)|;\(|:>|:<|;<|:-\)|;-\))', text)])
+    emot_del = re.sub(r'(?::\)|;\)|;\(|:>|:<|;<|:-\)|;-\))', '', text)
+    low = emot_del.lower()
+    numbers = re.sub(r'\d', '', low)
+    html = re.sub(r'<.*?>', '', numbers)
+    multiple_blank = re.sub(' +', ' ', html)
+    punct_marks = re.sub(r'[,;.\']', '', multiple_blank)
+    result = punct_marks + emot
+    return result
